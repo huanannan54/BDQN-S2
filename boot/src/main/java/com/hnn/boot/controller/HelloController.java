a22ab7c27e20a4771a5176e865b538d4b8c0535b
hnn <1589563832@qq.com>
2020-05-04 16:20:30 +0800

    重载

--- boot/src/main/java/com/hnn/boot/controller/HelloController.java
+++ boot/src/main/java/com/hnn/boot/controller/HelloController.java
@@ -15,12 +15,31 @@ public class HelloController {
 	}
 	
 	@RequestMapping("/2")
-//	@ResponseBody //将返回的格式 转成json字符 给前端。
+//	@ResponseBody //如果没有这个 注解，就代表 "hello" 是jsp 页面的名字。
 	public String hello2() {
 		System.out.println("进入hello2方法");
 		System.out.println("进入hello2方法");
 		System.out.println("进入hello2方法");
-		return "hello";
+		return "hello";//返回页面名字为 hello的文件。
+	}
+	
+	public static void aa(int a){
+		System.out.println("我要吃饭");
+	}
+	/**
+	 * 方法重载 跟返回类型 没关系
+	 * 参数不能一样
+	 */
+	public static String aa(String i){
+		System.out.println("我要喝水");
+		return "";
+	}
+	
+	public static void main(String[] args) {
+		HelloController.aa(1);
+		HelloController.aa("");
+		
+		
 	}
 	
 }
