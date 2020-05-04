d57b2bbb11e030c8a8ceac4503bc4fee5c59ea8f
LAPTOP-M8MGPD18\ASUS <1589563832@qq.com>
2020-05-04 15:35:02 +0800

    # 第一次新建项目

---
+++ boot/src/main/java/com/hnn/boot/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.hnn.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+	@RequestMapping("/1")
+	@ResponseBody //将返回的格式 转成json字符 给前端。
+	public String hello() {
+		return "你好，这是我的第一个boot 项目";
+	}
+	
+	@RequestMapping("/2")
+//	@ResponseBody //将返回的格式 转成json字符 给前端。
+	public String hello2() {
+		System.out.println("进入hello2方法");
+		return "hello";
+	}
+	
+}
---
+++ boot/src/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>hello 你好</title>
+</head>
+<body>
+<p>你好欢迎来到我的第一个jsp页面</p>
+</body>
+</html>
